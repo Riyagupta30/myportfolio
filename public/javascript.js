066ef7a31ef05cde4579c1db9e5d95ff343b3fa6
Riya <riyaktl777@gmail.com>
2021-08-24 12:26:20 +0530

    first commit

---
+++ public/javascript.js
@@ -0,0 +1,6 @@
+// $('.portfolio .button-group #btn1').trigger('click');
+
+//     $('.portfolio .grid .test-popup-link').magnificPopup({
+//         type: 'image',
+//         gallery: { enabled: true }
+//     });
